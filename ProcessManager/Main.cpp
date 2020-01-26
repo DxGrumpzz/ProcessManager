fa32afbc77f2b75e1005ae022216eaf7bdf79f91
Josef Lintz <yosi1.lints@gmail.com>
2020-01-26 16:44:01 +0200

    + Added very basic file process reader

--- ProcessManager/Main.cpp
+++ ProcessManager/Main.cpp
@@ -2,7 +2,8 @@
 #include <processthreadsapi.h >
 #include <cstring>
 #include <sstream>
-#include <sstream>
+#include <fstream>
+#include <vector>
 
 std::string GetErrorString(DWORD error)
 {
@@ -20,6 +21,75 @@ std::string GetErrorString(DWORD error)
 };
 
 
+struct ProcessModel
+{
+
+    std::string ProcessName;
+    std::string ProcessArgs;
+    
+
+    ProcessModel(std::string _processName, std::string _processArgs) :
+        ProcessName(_processName),
+        ProcessArgs(_processArgs)
+    {
+    };
+
+};
+        
+
+// Returns a list of ProcessModel which contain name and arguments of a process 
+std::vector<ProcessModel> GetProcessList(const char* filename)
+{
+    // Stores the list of processes as a ProcessModel struct
+    std::vector<ProcessModel> processes;
+
+    // The processes file
+    std::ifstream file(filename);
+
+    // If file is invalid
+    if (!file)
+    {
+        std::string error = "File error ";
+        error.append(filename);
+
+        throw new std::runtime_error(error);
+    };
+
+    // This is absolute aids. 
+    // This will improve
+
+    // Iterate through the file by line by line
+    for (std::string line; std::getline(file, line);)
+    {
+        // If current line specifies a process name
+        if (line == "[Process]")
+        {
+            // Get the name of the process
+            std::string processName; 
+            file >> processName;
+
+            // Add "new" process to the list without arguments
+            processes.emplace_back(processName, "");
+        }
+        // If current line specifies a processes args
+        else if (line == "[Args]")
+        {
+            // Get args 
+            std::string processArgs;
+            file >> processArgs;
+
+            // Because of the way command arguments are interpreted a space must be inserted in the beggining of the string
+            processArgs.insert(processArgs.begin(), ' ');
+
+            // Get last process created, And set it's argument
+            auto process= (processes.end() - 1);
+            process->ProcessArgs = processArgs;
+        };
+
+    };
+
+    return processes;
+};
 int main()
 {
     const char* path = "C:\\WINDOWS\\system32\\notepad.exe";
