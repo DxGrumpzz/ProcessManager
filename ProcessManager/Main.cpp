2f958ef0cc0df51edfc96c4ac7bb7cbdea61522d
Josef Lintz <yosi1.lints@gmail.com>
2020-02-23 12:21:22 +0200

    + Added comments

--- ProcessManager/Main.cpp
+++ ProcessManager/Main.cpp
@@ -33,45 +33,49 @@ std::wstring GetErrorStringW(DWORD error)
 const wchar_t* windowTitle = L"Process manager";
 const wchar_t* windowClassName = L"DesktopApp";
 
-HINSTANCE hInstance;
+// The button that runs the processes
+HWND _createProcessesButton;
 
-HWND button;
-HWND button2;
+// The button that closes the processes
+HWND _closeProcessesButton;
 
+// A list of labels that contain the names of the processes
 std::vector<HWND> _processesLabels;
 
 
-
+// The MainWindow function procedure
 LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
         case WM_CLOSE:
         {
+            // Send a quit message to this window 
             PostQuitMessage(0);
-            for (ProcessModel& process : ProcessManager::ProcessList)
-            {
-                ProcessManager::CloseProcess(process);
-            };
+
+            // Close processes
+            ProcessManager::CloseEveryProcess();
+
             return 0;
         };
 
 
         case  WM_COMMAND:
         {
+            // If the commnad is a mouse button click
             if (HIWORD(wParam) == BN_CLICKED)
             {
+                // Get window ID
                 WORD controlID = LOWORD(wParam);
 
+                
                 switch (controlID)
                 {
                     // Create process button
                     case 1:
                     {
-                        for (ProcessModel& process : ProcessManager::ProcessList)
-                        {
-                            ProcessManager::RunProcess(process);
-                        };
+                        // Run the processes
+                        ProcessManager::RunEveryProcess();
 
                         return TRUE;
                     };
@@ -79,17 +83,11 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM 
                     // Abort process button
                     case 2:
                     {
-                        for (ProcessModel& process : ProcessManager::ProcessList)
-                        {
-                            ProcessManager::CloseProcess(process);
-                        };
+                        // Close every process
+                        ProcessManager::CloseEveryProcess();
 
                         return TRUE;
                     };
-
-
-                    default:
-                        return DefWindowProcW(hWnd, message, wParam, lParam);
                 };
             };
 
@@ -99,21 +97,27 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM 
 
         case WM_SIZE:
         {
+            // Get the window's new size
             const int newWidth = LOWORD(lParam);
 
+            // Because the labels are vertically stacked, 
+            // A counter is needed to calculate the correct Y position relative to the label's height
             int counter = 0;
+
             for (const HWND& _hwnd : _processesLabels)
             {
+                // Get the label's window rect
                 RECT windowRect;
                 GetWindowRect(_hwnd, &windowRect);
 
+                // Calculate the label's width, height, and positions
                 const int windowWidth = windowRect.right - windowRect.left;
                 const int windowHeight = windowRect.bottom - windowRect.top;
 
                 const int newX = (newWidth - windowWidth) - 10;
                 const int newY = ((windowHeight + 4) * counter) + 10;
 
-
+                // "Append" the label to the right of the screen
                 SetWindowPos(_hwnd, NULL,
                              newX, newY,
                              0, 0,
@@ -122,7 +126,7 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM 
                 counter++;
             };
 
-            return 1;
+            return 0;
         }
 
 
@@ -132,6 +136,7 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM 
 };
 
 
+// Create the app's main window
 HWND CreateMainWindow(const HINSTANCE& hInstance)
 {
     WNDCLASSEXW windowClass = { 0 };
@@ -143,7 +148,7 @@ HWND CreateMainWindow(const HINSTANCE& hInstance)
     windowClass.hbrBackground = CreateSolidBrush(RBG_UNIFORM(0xE1));
     windowClass.hCursor = LoadCursorW(NULL, IDC_ARROW);
 
-
+    // Register the window
     ATOM registerClassResult = RegisterClassExW(&windowClass);
 
     if (registerClassResult == 0)
@@ -157,7 +162,7 @@ HWND CreateMainWindow(const HINSTANCE& hInstance)
     };
 
 
-
+    // Create the actuall window
     HWND windowHWND = CreateWindowExW(NULL,
                                       windowClassName,
                                       windowTitle,
@@ -170,13 +175,13 @@ HWND CreateMainWindow(const HINSTANCE& hInstance)
                                       NULL);
 
 
-
     return windowHWND;
 }
 
+// Creates the buttons that run and close  the processes
 void CreateButtons(const HWND& windowHWND, const HINSTANCE& hInstance)
 {
-    button = CreateWindowExW(NULL,
+    _createProcessesButton = CreateWindowExW(NULL,
                              L"BUTTON",
                              L"Run processes",
                              WS_BORDER | WS_CHILD,
@@ -190,9 +195,9 @@ void CreateButtons(const HWND& windowHWND, const HINSTANCE& hInstance)
                              NULL);
 
     // Set button cursor
-    SetClassLongPtrW(button, GCLP_HCURSOR, (LONG_PTR)LoadCursorW(NULL, IDC_HAND));
+    SetClassLongPtrW(_createProcessesButton, GCLP_HCURSOR, (LONG_PTR)LoadCursorW(NULL, IDC_HAND));
 
-    button2 = CreateWindowExW(NULL,
+    _closeProcessesButton = CreateWindowExW(NULL,
                               L"BUTTON",
                               L"Stop processes",
                               WS_BORDER | WS_CHILD,
@@ -206,39 +211,48 @@ void CreateButtons(const HWND& windowHWND, const HINSTANCE& hInstance)
                               NULL);
 
 
-    ShowWindow(button, SW_SHOW);
-    ShowWindow(button2, SW_SHOW);
+    // Show the buttons
+    ShowWindow(_createProcessesButton, SW_SHOW);
+    ShowWindow(_closeProcessesButton, SW_SHOW);
 }
 
+// Create the Process labels 
 void CreateProcessLabels(const HWND& windowHWND, const HINSTANCE& hInstance)
 {
     int longestProcessName = 0;
 
+    // Find the process with the longest name and set "longestProcessName" to it's string size
     for (const ProcessModel& process : ProcessManager::ProcessList)
     {
         if (process.ProcessName.size() > longestProcessName)
             longestProcessName = (int)process.ProcessName.size();
     };
 
-    const int  CHAR_MULTIPLIER = 8;
+    // An arbitrary character multipler, used to calculate the label's window width
+    constexpr int CHAR_MULTIPLIER = 8;
 
+    // The width of the text
     const int TEXT_WIDTH = longestProcessName * CHAR_MULTIPLIER;
 
-
-    int index = 0;
+    // Because the labels are vertically stacked, 
+    // A counter is needed to calculate the correct Y position relative to the label's height
+    int counter = 0;
+    
     for (const ProcessModel& process : ProcessManager::ProcessList)
     {
+        // Find the window height by checking if there is a newline character and multiple it by some value (20)
         int TEXT_HEIGHT = (int)(std::count(process.ProcessName.begin(), process.ProcessName.end(), L'\n') + 1) * 20;
 
-        const int TEXT_X_POSITION = abs(500 - TEXT_WIDTH) - 15;
-        const int TEXT_Y_POSITION = TEXT_HEIGHT * index;
-        const long long id = (long long)index + 3L;
+        // Set an ID for evey label
+        const long long id = (long long)counter + 3L;
 
         HWND textBlock = CreateWindowExW(NULL,
                                          L"STATIC",
                                          process.ProcessName.c_str(),
                                          WS_CHILD | SS_CENTER | SS_NOTIFY,
-                                         TEXT_X_POSITION, TEXT_Y_POSITION + (index * 4),
+                                         // When the window is created the OS sends a WM_SIZE message that will be handled,
+                                         // So there is no need to set X and Y
+                                         0, 0,
                                          TEXT_WIDTH, TEXT_HEIGHT,
                                          windowHWND,
                                          (HMENU)id,
@@ -246,22 +260,24 @@ void CreateProcessLabels(const HWND& windowHWND, const HINSTANCE& hInstance)
                                          NULL);
 
 
+        // Add the label to the list
         _processesLabels.push_back(textBlock);
 
+        // Show the label
         ShowWindow(textBlock, SW_SHOW);
 
-        index++;
+        counter++;
     };
 }
-
 
 
 // _In_opt_ nad _In_ are something called SAL annotations, They mean that a parameter maybe be passed as NULL
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
 {
-
+    // Create the main window
     HWND windowHWND = CreateMainWindow(hInstance);
 
+    // Handle window creation errors
     if (windowHWND == 0)
     {
         std::wstring error = GetErrorStringW(GetLastError());
@@ -272,34 +288,49 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
         return 1;
     };
 
-
+    // Create process buttons
     CreateButtons(windowHWND, hInstance);
 
+    // Setup ProcessManager 
     ProcessManager::GetProcessListFromFile();
 
+    // Create the process labels
     CreateProcessLabels(windowHWND, hInstance);
 
-
+    // Show the main window
     ShowWindow(windowHWND, nShowCmd);
-    UpdateWindow(windowHWND);
 
+
+    // Windows message loop
     MSG message;
+    
+    // Continuously try and get message
     while (1)
     {
+        // Peek message returns 1 if there is a message(s), 
+        // If there are no messages it will return 0.
+        // So we continually loop as long as there are messages in queue
         while (PeekMessageW(&message, NULL, 0, 0, PM_REMOVE))
         {
+            // If the message is a keystroke get the key's character value
             TranslateMessage(&message);
+
+            // Send the message to the Window procedure function
             DispatchMessageW(&message);
         };
 
+        // To exit the infinite loop check if the current message was a quit message
         if (message.message == WM_QUIT)
             break;
 
+        // Hide the processes. 
+        // Why is this here ? read ProcessManager::RunProcess doc
         for (ProcessModel& process : ProcessManager::ProcessList)
         {
-            ProcessManager::HideProcess(process, windowHWND);
+            ProcessManager::HideProcess(process);
         };
 
+        // Because this is an infinite loop a 1ms thread delay is a must
         Sleep(1);
     };
 
--- ProcessManager/ProcessManager.h
+++ ProcessManager/ProcessManager.h
@@ -5,28 +5,35 @@
 
 #include "ProcessModel.h"
 
-
+// A class that is responsible for interaction with the ProcessModel
 class ProcessManager
 {
 
 public:
 
+    // The list of processes that will be ran
     static std::vector<ProcessModel> ProcessList;
 
 
 private:
 
+    // A WinEventHook function used to get a process HWND's
     static void CALLBACK WinEventHookCallback(HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd, LONG idObject, LONG idChild, DWORD idEventThread, DWORD dwmsEventTime)
     {
+        // Get the current process ID
         DWORD processId;
         GetWindowThreadProcessId(hwnd, &processId);
 
         for (ProcessModel& process : ProcessList)
         {
+            // Match the ID with the created process
             if (process.ProcessInfo.dwProcessId == processId)
             {
-                process.Creating = true;
+                // Add the process HWND to the process handles list
                 process.handles.push_back(hwnd);
+
+                // Set creating flag
+                process.Creating = true;
             };
         };
     };
@@ -35,24 +42,42 @@ private:
 
 public:
 
+    // Runs a single process
     static BOOL RunProcess(ProcessModel& process)
     {
+        // Fix process arguments string if needed
         NormalizeArgs(process);
 
-        if (!CreateProcessW(process.ProcessName.c_str(), const_cast<wchar_t*>(process.ProcessArgs.c_str()), NULL, NULL, FALSE, CREATE_SUSPENDED | CREATE_NO_WINDOW, NULL, NULL, &process.info, &process.ProcessInfo))
+        // Create/run the process
+        if (!CreateProcessW(process.ProcessName.c_str(),
+            // Because the argument string will be appended to the default path argument 
+            // a const cast must be used to convert the LPCWSTR to a LPWSTR
+            const_cast<wchar_t*>(process.ProcessArgs.c_str()), 
+            NULL,NULL, 
+            FALSE,
+            // Create a suspended process, A process that will start paused until ResumeThread is called
+            // Run the process without a window, Only works on Console app for some reason 
+            CREATE_SUSPENDED | CREATE_NO_WINDOW, 
+            NULL,
+            NULL, 
+            &process.info, 
+            &process.ProcessInfo))
         {
-            CloseHandle(process.ProcessInfo.hProcess);
-            CloseHandle(process.ProcessInfo.hThread);
+            // If process creation failed.
+
+            // Close the process handles
+            CleanupProcessHandles(process);
 
             return FALSE;
         };
-
+        
+        // Setup the hook function so it will only call the function when the process has finished initialization
         process.Hook = SetWinEventHook(EVENT_OBJECT_CREATE, EVENT_OBJECT_CREATE, NULL, WinEventHookCallback, process.ProcessInfo.dwProcessId, 0, WINEVENT_OUTOFCONTEXT);
 
+        // After hooking the process resume normal execution
         if (!ResumeThread(process.ProcessInfo.hThread))
         {
-            CloseHandle(process.ProcessInfo.hProcess);
-            CloseHandle(process.ProcessInfo.hThread);
+            CleanupProcessHandles(process);
 
             return FALSE;
         };
@@ -60,22 +85,27 @@ public:
         return TRUE;
     };
 
+    // Closes a single process
     static BOOL CloseProcess(ProcessModel& process)
     {
+        // Get process exit code
         DWORD exitCode;
         GetExitCodeProcess(process.ProcessInfo.hProcess, &exitCode);
 
+        // If the process is running
         if (exitCode == STILL_ACTIVE)
         {
+            // Unhook the WinEvent from the process
+            UnhookWinEvent(process.Hook);
+           
+            // Close the process
             if (!TerminateProcess(process.ProcessInfo.hProcess, 0))
                 return FALSE;
 
-            if (!CloseHandle(process.ProcessInfo.hProcess) &&
-                !CloseHandle(process.ProcessInfo.hThread))
+            if(!CleanupProcessHandles(process));
                 return FALSE;
-
-            UnhookWinEvent(process.Hook);
         }
+        // Don't do anything if the process is closed
         else
         {
             return FALSE;
@@ -85,7 +115,7 @@ public:
     }
 
 
-
+    // Creates and runs every process in the list
     static void RunEveryProcess()
     {
         for (ProcessModel& process : ProcessManager::ProcessList)
@@ -94,7 +124,7 @@ public:
         };
     };
 
-
+    // Closes every process in the list
     static void CloseEveryProcess()
     {
         for (ProcessModel& process : ProcessManager::ProcessList)
@@ -104,23 +134,21 @@ public:
     };
 
 
-
+    // Hides a process (Only the visible window, nothing sketchy)
     static void HideProcess(ProcessModel& process)
     {
+        // If the process is in "creation" mode
         if (process.Creating == true)
         {
+            // Go through every handle, and run ShowWindow with SW_HIDE
             for (const HWND& hwnd : process.handles)
             {
-                ShowWindowAsync(hwnd, SW_HIDE);
+                ShowWindow(hwnd, SW_HIDE);
             };
 
+            // Set creating flag
             process.Creating = false;
         };
-    };
-
-    static void CreateProcessModel(std::wstring processName, std::wstring processArgs)
-    {
-        ProcessManager::ProcessList.emplace_back(processName, processArgs);
     };
 
 
@@ -188,12 +216,25 @@ public:
 
 private:
 
+    // Normalizes the argument string, Just in case
     static void NormalizeArgs(ProcessModel& process)
     {
+        // In windows to pass an agument string to a process 
+        // The string MUST contain a space in the beggining.
+        // This is because by default windows passes a path argument to every running process
+
         if (process.ProcessArgs[0] != L' ')
         {
             process.ProcessArgs.insert(0, L" ");
         };
+    }
+
+    // Closes an opened process' handles
+    static BOOL CleanupProcessHandles(ProcessModel& process)
+    {
+        if (!CloseHandle(process.ProcessInfo.hProcess) &&
+            !CloseHandle(process.ProcessInfo.hThread))
+            return FALSE;
     }
 
 };
