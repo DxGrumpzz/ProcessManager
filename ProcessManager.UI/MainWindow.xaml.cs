f2f8b06d8afc3d7c2e1bc5596b6cf321e04e57fc
Josef Lintz <yosi1.lints@gmail.com>
2020-02-25 09:16:17 +0200

    - Removed  unnecessary WinApi calls

--- ProcessManager.UI/MainWindow.xaml.cs
+++ ProcessManager.UI/MainWindow.xaml.cs
@@ -1,76 +1,18 @@
 ﻿namespace ProcessManager.UI
 {
-    using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Runtime.InteropServices;
     using System.Windows;
     using System.Windows.Controls;
 
-    public static class ProcessExtensions
-    {
-        private static class Win32
-        {
-            [DllImport("user32.dll", CharSet = CharSet.Auto)]
-            public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
-
-            [return: MarshalAs(UnmanagedType.Bool)]
-            [DllImport("user32.dll", SetLastError = true)]
-            public static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
-
-            public delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
-            [DllImport("user32.dll")]
-            public static extern bool EnumThreadWindows(uint dwThreadId, EnumThreadDelegate lpfn, IntPtr lParam);
-
-        }
-
-        public static IntPtr SendMessage(this Process p, out IntPtr hwnd, UInt32 msg, IntPtr wParam, IntPtr lParam)
-        {
-            hwnd = p.WindowHandles().FirstOrDefault();
-
-            if (hwnd != IntPtr.Zero)
-                return Win32.SendMessage(hwnd, msg, wParam, lParam);
-            else
-                return IntPtr.Zero;
-        }
-
-        //Posts a message to the first enumerated window in the first enumerated thread with at least one window, and returns the handle of that window through the hwnd output parameter if such a window was enumerated.  If a window was enumerated, the return value is the return value of the PostMessage call, otherwise the return value is false.
-        public static bool PostMessage(this Process p, out IntPtr hwnd, UInt32 msg, IntPtr wParam, IntPtr lParam)
-        {
-            hwnd = p.WindowHandles().FirstOrDefault();
-            if (hwnd != IntPtr.Zero)
-                return Win32.PostMessage(hwnd, msg, wParam, lParam);
-            else
-                return false;
-        }
-
-        public static IEnumerable<IntPtr> WindowHandles(this Process process)
-        {
-            var handles = new List<IntPtr>();
-
-                foreach (ProcessThread thread in process.Threads)
-                {
-                    //process.WaitForInputIdle();
-
-                    Win32.EnumThreadWindows((uint)thread.Id, (hWnd, lParam) =>
-                    {
-                        handles.Add(hWnd); 
-                        return true;
-                    },
-                    IntPtr.Zero);
-                }
-            return handles;
-        }
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
         private const string DLL = "ProcessManager.Core.dll";
         private class ProcessModel
         {
@@ -106,7 +48,6 @@
             _processList.ForEach(process => AddProcessToList(process));
 
             Initialize();
-
         }
 
 
