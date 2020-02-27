cb89e8b7e63ab62629f3dbcdd289aa97fb31f630
Josef Lintz <yosi1.lints@gmail.com>
2020-02-27 14:28:16 +0200

    + Added ViewModel for the MainWindow

---
+++ ProcessManager.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace ProcessManager.UI
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MainWindowViewModel
+    {
+        public List<ProcessItemViewModel> Processes { get; }
+
+        public MainWindowViewModel(IEnumerable<ProcessModel> processes)
+        {
+            Processes = new List<ProcessItemViewModel>(processes.Select(process =>
+            new ProcessItemViewModel()
+            { 
+                Process = process,
+            }));
+
+
+        }
+    };
+};
