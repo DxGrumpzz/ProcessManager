345f8bf6c4c3b59c3117e1f11f63ce10275fb236
Josef Lintz <yosi1.lints@gmail.com>
2020-02-27 14:07:55 +0200

    + Added ProcessItemViewModel

---
+++ ProcessManager.UI/ViewModels/ProcessItemViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace ProcessManager.UI
+{
+    using System.Windows.Input;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProcessItemViewModel
+    {
+
+        public ProcessModel Process { get; set; }
+
+        public string ProcessName => Process.ProcessName;
+
+
+        public ICommand RunProcessCommand { get; }
+        public ICommand CloseProcessCommand { get; }
+
+
+        public ProcessItemViewModel()
+        {
+            RunProcessCommand = new RelayCommand(ExecuteRunProcessCommand);
+            CloseProcessCommand= new RelayCommand(ExecuteCloseProcessCommand);
+        }
+
+
+        private void ExecuteRunProcessCommand()
+        {
+            
+        }
+
+        private void ExecuteCloseProcessCommand()
+        {
+
+        }
+
+    };
+};
