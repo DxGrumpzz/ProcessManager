c7ff0a7bae3dc9a21e21d9bdac2e00d0fb696c72
Josef Lintz <yosi1.lints@gmail.com>
2020-03-01 14:56:27 +0200

    - Removed GetProcessesFromFile from app.xaml

--- ProcessManager.UI/App.xaml.cs
+++ ProcessManager.UI/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace ProcessManager.UI
+namespace ProcessManager.UI
 {
     using System;
     using System.Collections.Generic;
@@ -44,31 +44,12 @@
             });
         }
 
-        private List<ProcessModel> GetProcessesFromFile(string filePath = "Processes.txt")
-        {
-            var lines = File.ReadAllLines(filePath)
-                .Where(line => string.IsNullOrWhiteSpace(line) == false);
 
-            List<ProcessModel> processList = new List<ProcessModel>();
-
-            for (int a = 0; a < lines.Count(); a++)
+        private static void SetupDI()
             {
                 string currentLine = lines.ElementAt(a).ToLower();
 
-                if (currentLine == "[process]")
-                {
-                    processList.Add(new ProcessModel()
-                    {
-                        ProcessName = lines.ElementAt(++a),
-                    });
-                }
-                else if (currentLine == "[args]")
-                {
-                    processList.Last().ProcessArgs = lines.ElementAt(++a);
-                };
-            };
-
-            return processList;
+            DI.ProcessList = new List<ProcessModel>(DI.ProcessLoader.GetProcessesFromFile("Processes.txt"));
         }
 
     };
