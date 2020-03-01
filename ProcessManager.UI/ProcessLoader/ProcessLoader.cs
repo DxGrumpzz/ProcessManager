279aec7f05078fdea816d53d012714ec7364f803
Josef Lintz <yosi1.lints@gmail.com>
2020-03-01 14:54:50 +0200

    + Added ProcessLoader implementation

---
+++ ProcessManager.UI/ProcessLoader/ProcessLoader.cs
@@ -0,0 +1,40 @@
+﻿namespace ProcessManager.UI
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProcessLoader : IProcessLoader
+    {
+        public IEnumerable<ProcessModel> GetProcessesFromFile(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath)
+                .Where(line => string.IsNullOrWhiteSpace(line) == false);
+
+            List<ProcessModel> processList = new List<ProcessModel>();
+
+            for (int a = 0; a < lines.Count(); a++)
+            {
+                string currentLine = lines.ElementAt(a).ToLower();
+
+                if (currentLine == "[process]")
+                {
+                    processList.Add(new ProcessModel()
+                    {
+                        ProcessName = lines.ElementAt(++a),
+                    });
+                }
+                else if (currentLine == "[args]")
+                {
+                    processList.Last().ProcessArgs = lines.ElementAt(++a);
+                };
+            };
+
+            return processList;
+        }
+
+    };
+};
