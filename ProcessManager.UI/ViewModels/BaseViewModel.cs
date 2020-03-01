64e9b23f55a1d84504ba6ffad4829d60396a9881
Josef Lintz <yosi1.lints@gmail.com>
2020-03-01 13:58:33 +0200

    + Added BaseViewModel

---
+++ ProcessManager.UI/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ProcessManager.UI
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName()]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    };
+};
