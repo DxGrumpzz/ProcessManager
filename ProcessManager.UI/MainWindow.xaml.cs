8b1cf5c680708d83d1cfbbc05a5d60f0b060a3db
Josef Lintz <yosi1.lints@gmail.com>
2020-03-01 13:54:57 +0200

    - Removed process events from MainWindow code-behind

--- ProcessManager.UI/MainWindow.xaml.cs
+++ ProcessManager.UI/MainWindow.xaml.cs
@@ -19,58 +19,17 @@
             DataContext = viewModel;
         }
 
-        private void Button_Run_Processes_Click(object sender, RoutedEventArgs e)
-        {
-            DI.ProcessList.ForEach(process =>
-            {
-                var processID = CoreDLL.RunProcess(process.ProcessName, process.ProcessArgs);
-                process.ProcessID = processID;
-            });
-        }
-
-        private void Button_Close_Processes_Click(object sender, RoutedEventArgs e)
-        {
-            DI.ProcessList.ForEach(process =>
-            {
-                CoreDLL.CloseProcess(process.ProcessID);
-            });
-        }
-
-
         private void Window_Closing(object sender, CancelEventArgs e)
         {
             DI.ProcessList.ForEach(process =>
             {
-                CoreDLL.CloseProcess(process.ProcessID);
+                if (process.IsRunning == true)
+                {
+                    CoreDLL.CloseProcess(process.ProcessID);
+                    process.ProcessID = 0;
+                };
             });
         }
 
-
-        private void RunProcessButtonClick(object sender, RoutedEventArgs e)
-        {
-            FrameworkElement button = (FrameworkElement)sender;
-            ProcessModel process = (ProcessModel)button.DataContext;
-
-            if (process.IsRunning == false)
-            {
-                ulong processID = CoreDLL.RunProcess(process.ProcessName, process.ProcessArgs);
-                process.ProcessID = processID;
-            };
-        }
-
-        private void CloseProcessButtonClick(object sender, RoutedEventArgs e)
-        {
-            FrameworkElement button = (FrameworkElement)sender;
-            ProcessModel process = (ProcessModel)button.DataContext;
-
-
-            if (process.IsRunning == true)
-            {
-                CoreDLL.CloseProcess(process.ProcessID);
-                process.ProcessID = 0;
-            };
-        }
-
-        
     };
 };
