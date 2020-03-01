5b5c4b3b6a8f2867e082ae1655088fae264083a0
Josef Lintz <yosi1.lints@gmail.com>
2020-03-01 13:01:18 +0200

    * Changed MainWindow ProcessList to be bound to a propery

--- ProcessManager.UI/MainWindow.xaml.cs
+++ ProcessManager.UI/MainWindow.xaml.cs
@@ -1,11 +1,6 @@
 ﻿namespace ProcessManager.UI
 {
-    using System;
-    using System.Collections.Generic;
     using System.ComponentModel;
-    using System.IO;
-    using System.Linq;
-    using System.Runtime.InteropServices;
     using System.Windows;
 
     /// <summary>
@@ -13,39 +8,20 @@
     /// </summary>
     public partial class MainWindow : Window
     {
-      
-        List<ProcessModel> _processList;
-
-
-
         public MainWindow()
         {
-
-            if (File.Exists("Processes.txt") == false)
-            {
-                MessageBox.Show("Unable to find Processes.txt");
-                Environment.Exit(1);
-                return;
-            };
-
-
             InitializeComponent();
-
-            // Setup process list
-            _processList = GetProcessesFromFile();
-
-            // Display processes
-            _processList.ForEach(process => ProcessList.Items.Add(process));
-
-
-            CoreDLL.Initialize();
         }
 
-
+        public MainWindow(MainWindowViewModel viewModel) :
+            this()
+        {
+            DataContext = viewModel;
+        }
 
         private void Button_Run_Processes_Click(object sender, RoutedEventArgs e)
         {
-            _processList.ForEach(process =>
+            DI.ProcessList.ForEach(process =>
             {
                 var processID = CoreDLL.RunProcess(process.ProcessName, process.ProcessArgs);
                 process.ProcessID = processID;
@@ -54,19 +30,21 @@
 
         private void Button_Close_Processes_Click(object sender, RoutedEventArgs e)
         {
-            _processList.ForEach(process =>
+            DI.ProcessList.ForEach(process =>
             {
                 CoreDLL.CloseProcess(process.ProcessID);
             });
         }
 
+
         private void Window_Closing(object sender, CancelEventArgs e)
         {
-            _processList.ForEach(process =>
+            DI.ProcessList.ForEach(process =>
             {
                 CoreDLL.CloseProcess(process.ProcessID);
             });
         }
+
 
         private void RunProcessButtonClick(object sender, RoutedEventArgs e)
         {
@@ -91,34 +69,6 @@
                 CoreDLL.CloseProcess(process.ProcessID);
                 process.ProcessID = 0;
             };
-        }
-
-
-        private List<ProcessModel> GetProcessesFromFile(string filePath = "Processes.txt")
-        {
-            var lines = File.ReadAllLines(filePath)
-                .Where(line => string.IsNullOrWhiteSpace(line) == false);
-
-            List<ProcessModel> processList = new List<ProcessModel>();
-
-            for (int a = 0; a < lines.Count(); a++)
-            {
-                string currentLine = lines.ElementAt(a).ToLower();
-
-                if (currentLine == "[process]")
-                {
-                    processList.Add(new ProcessModel()
-                    {
-                        ProcessName = lines.ElementAt(++a),
-                    });
-                }
-                else if (currentLine == "[args]")
-                {
-                    processList.Last().ProcessArgs = lines.ElementAt(++a);
-                };
-            };
-
-            return processList;
         }
 
         
