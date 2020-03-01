e18b540f602aab29053cbd154b5cf80908b0a4cd
Josef Lintz <yosi1.lints@gmail.com>
2020-03-01 13:58:17 +0200

    + Added Process operations to MainWindowViewModel

--- ProcessManager.UI/ViewModels/MainWindowViewModel.cs
+++ ProcessManager.UI/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿namespace ProcessManager.UI
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Windows.Input;
 
 
     /// <summary>
@@ -11,15 +13,53 @@
     {
         public List<ProcessItemViewModel> Processes { get; }
 
+
+        public ICommand RunProcessesCommnad { get; }
+        public ICommand CloseProcessesCommnad { get; }
+ 
         public MainWindowViewModel(IEnumerable<ProcessModel> processes)
         {
             Processes = new List<ProcessItemViewModel>(processes.Select(process =>
             new ProcessItemViewModel()
-            { 
+            {
                 Process = process,
             }));
 
+            RunProcessesCommnad = new RelayCommand(ExecuteRunProcessesCommnad);
+            CloseProcessesCommnad = new RelayCommand(ExecuteCloseProcessesCommnad);
+        }
 
+
+        private void ExecuteRunProcessesCommnad()
+        {
+            Processes.ForEach(processVM =>
+            {
+                ProcessModel process = processVM.Process;
+
+                if (process.IsRunning == false)
+                {
+                    ulong processID = CoreDLL.RunProcess(process.ProcessName, process.ProcessArgs);
+                    process.ProcessID = processID;
+
+                    processVM.ProcessRunning = processID != 0 ? true : false;
+                };
+            });
+        }
+
+        private void ExecuteCloseProcessesCommnad()
+        {
+            Processes.ForEach(processVM =>
+            {
+                ProcessModel process = processVM.Process;
+                
+                if (process.IsRunning == true)
+                {
+                    CoreDLL.CloseProcess(process.ProcessID);
+                    process.ProcessID = 0;
+
+                    processVM.ProcessRunning = false;
+                };
+            });
         }
     };
 };
