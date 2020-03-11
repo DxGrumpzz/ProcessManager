5531233a832ca9cb5bbd8a1a1781fd89752fa670
Josef Lintz <yosi1.lints@gmail.com>
2020-03-11 12:06:04 +0200

    + Added StackPanelAttachedProperties class

---
+++ ProcessManager.UI/AttachedProperties/StackPanelAttachedProperties.cs
@@ -0,0 +1,56 @@
+﻿namespace ProcessManager.UI
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+
+
+    /// <summary>
+    /// Applies a single uniform margin to every control of a panel
+    /// </summary>
+    public static class UniformMargin
+    {
+        public static Thickness Margin { get; set; }
+
+        public static Thickness GetMargin(DependencyObject obj)
+        {
+            return (Thickness)obj.GetValue(MarginProperty);
+        }
+
+        public static void SetMargin(DependencyObject obj, Thickness value)
+        {
+            obj.SetValue(MarginProperty, value);
+        }
+
+        public static readonly DependencyProperty MarginProperty =
+            DependencyProperty.RegisterAttached(
+                nameof(Margin),
+                typeof(Thickness),
+                typeof(UniformMargin),
+                new PropertyMetadata(default(Thickness), MarginChangedCallback));
+
+        private static void MarginChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // Don't do anything if the control isn't a panel
+            if (!(d is Panel panel))
+                return;
+
+            // Unironically using a local function instead of a variable
+            void eventHandler(object sender, EventArgs evnt)
+            {
+                // Apply a uniform margin for every control in panel
+                foreach (Control control in panel.Children)
+                {
+                    control.Margin = (Thickness)e.NewValue;
+                };
+
+                // Unhook the event after finishing
+                panel.Initialized -= eventHandler;
+            };
+
+            // Hook  the event
+            panel.Initialized += eventHandler;
+
+        }
+    };
+};
