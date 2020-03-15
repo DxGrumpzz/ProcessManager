34080d766bdb255eda7e6e4aaaf41737609897c4
Josef Lintz <yosi1.lints@gmail.com>
2020-03-15 18:49:03 +0200

    + Added value converter for boolean visibility

---
+++ ProcessManager.UI/ValueConverters/BooleanToVisibilityValueConverter.cs
@@ -0,0 +1,35 @@
+﻿namespace ProcessManager.UI
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BooleanToVisibilityValueConverter : BaseValueConverter<BooleanToVisibilityValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // If there is an argument
+            if (parameter != null)
+            {
+                // Try and convert to a boolean, if it fails will always return false
+                bool.TryParse(parameter as string, out bool invert);
+
+                if (invert == true)
+                {
+                    return !((bool)value) == true ? Visibility.Visible : Visibility.Hidden;
+                };
+            };
+
+            return ((bool)value) == true ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    };
+};
