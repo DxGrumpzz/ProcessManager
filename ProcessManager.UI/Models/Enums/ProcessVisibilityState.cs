9431b59607a9b219668952f9d3fac7b641a4eaca
Josef Lintz <yosi1.lints@gmail.com>
2020-03-17 11:40:25 +0200

    + Added ProcessVisibilityState enum

---
+++ ProcessManager.UI/Models/Enums/ProcessVisibilityState.cs
@@ -0,0 +1,24 @@
+﻿namespace ProcessManager.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// An enum for the visibility states of a process
+    /// </summary>
+    public enum ProcessVisibilityState
+    {
+
+        /// <summary>
+        /// The process is visible to the user
+        /// </summary>
+        Visible = 0,
+        
+        /// <summary>
+        /// The process is hidden from the user
+        /// </summary>
+        Hidden = 1,
+
+    };
+};
