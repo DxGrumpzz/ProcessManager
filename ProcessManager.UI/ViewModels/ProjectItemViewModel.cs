0b893f34311071ca5bd8cc310c2c252c8466d051
Josef Lintz <yosi1.lints@gmail.com>
2020-03-30 15:38:29 +0300

    + Added ProjectItem viewmodel

--- ProcessManager.UI/ViewModels/ProjectItemViewModel.cs
+++ ProcessManager.UI/ViewModels/ProjectItemViewModel.cs
@@ -1,8 +1,8 @@
 ﻿namespace ProcessManager.UI
 {
     using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using System.IO;
+    using System.Windows.Input;
 
 
     /// <summary>
@@ -10,12 +10,34 @@
     /// </summary>
     public class ProjectItemViewModel
     {
+        public static ProjectItemViewModel DesignInstance => new ProjectItemViewModel(new Project()
+        {
+            ProjectPath = $@"C:\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}",
+        });
+
 
         public Project Project { get; }
+
+        public ICommand GotoProjectViewCommnad { get; }
+        public ICommand GotoMainPageCommnad { get; }
+
 
         public ProjectItemViewModel(Project project)
         {
             Project = project;
+
+            GotoProjectViewCommnad = new RelayCommand(ExecuteGotoProjectViewCommnad);
+            GotoMainPageCommnad = new RelayCommand(ExecuteGotoMainPageommnad);
+        }
+
+        private void ExecuteGotoMainPageommnad()
+        {
+            DI.MainWindowViewModel.CurrentView = new MainView(DI.MainWindowViewModel);
+        }
+
+        private void ExecuteGotoProjectViewCommnad()
+        {
+            DI.MainWindowViewModel.CurrentView = new ProjectItemView(this);
         }
     };
 };
