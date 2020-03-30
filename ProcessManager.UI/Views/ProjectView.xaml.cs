305bc30e6eac40589be3b76c8139be150aefc76b
Josef Lintz <yosi1.lints@gmail.com>
2020-03-30 15:37:29 +0300

    + Added ProjectView

---
+++ ProcessManager.UI/Views/ProjectView.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcessManager.UI
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ProjectView.xaml
+    /// </summary>
+    public partial class ProjectItemView : UserControl
+    {
+        public ProjectItemView(ProjectItemViewModel projectItemViewModel)
+        {
+            DataContext = projectItemViewModel;
+            InitializeComponent();
+        }
+    }
+}
