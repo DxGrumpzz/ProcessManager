e941062af9cd386ccc7af60da2a75b5340b423d9
Josef Lintz <yosi1.lints@gmail.com>
2020-03-30 14:37:58 +0300

    + Added Project class

---
+++ ProcessManager.UI/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿namespace ProcessManager.UI
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+
+    public class Project
+    {
+        public string ProjectPath { get; set; }
+
+        public string ProjectPathWithConfig => Path.Combine(ProjectPath, "ProcessManger.Config.Json");
+
+        public IEnumerable<ProcessModel> ProcessList { get; set; }
+    }
+};
