1ae694388ca179cee8a0a8972ef836c0faab110b
Josef Lintz <yosi1.lints@gmail.com>
2020-04-07 18:12:15 +0300

    + Added comments to SystemTrayIcon

--- ProcessManager.UI/SystemTrayIcon.cs
+++ ProcessManager.UI/SystemTrayIcon.cs
@@ -1,33 +1,60 @@
 ﻿namespace ProcessManager.UI
 {
     using System;
-    using System.Linq;
 
+    /// <summary>
+    /// A class responsible for interaction with the TrayIcon
+    /// </summary>
     public class SystemTrayIcon
     {
-        private readonly string _icon;
+
+        /// <summary>
+        /// Path to the icon's bitmap
+        /// </summary>
+        private readonly string _iconPath;
+
+        /// <summary>
+        /// A handle to the main window
+        /// </summary>
         private readonly IntPtr _mainWindowHandle;
+
+        /// <summary>
+        /// The Icon's pointer/handle
+        /// </summary>
         private IntPtr _iconPointer;
 
-        public SystemTrayIcon(string icon, IntPtr mainWindowHandle)
+
+        public SystemTrayIcon(string iconPath, IntPtr mainWindowHandle)
         {
-            _icon = icon;
+            _iconPath = iconPath;
             _mainWindowHandle = mainWindowHandle;
         }
 
+        /// <summary>
+        /// *Creates* the icon and gives it some data
+        /// </summary>
+        /// <param name="systemTrayIconData"></param>
         public void CreateIcon(SystemTrayIconData[] systemTrayIconData)
         {
-            _iconPointer = CoreDLL.CreateSystemTrayIcon(_mainWindowHandle, _icon, systemTrayIconData, systemTrayIconData.Length);
+            // Call C++ dll and create the icon. 
+            _iconPointer = CoreDLL.CreateSystemTrayIcon(_mainWindowHandle, _iconPath, systemTrayIconData, systemTrayIconData.Length);
         }
 
+        /// <summary>
+        /// Shows the icon after it was created
+        /// </summary>
         public void ShowIcon()
         {
             CoreDLL.ShowSystemTrayIcon(_iconPointer);
         }
 
+        /// <summary>
+        /// Removes the icon from System Tray. Should be called when app closes
+        /// </summary>
         public void RemoveIcon()
         {
             CoreDLL.RemoveSystemTrayIcon(_iconPointer);
         }
+
     };
 };
