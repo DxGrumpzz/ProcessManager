6a38818449204970543f60884816a72102ed28c1
Josef Lintz <yosi1.lints@gmail.com>
2020-04-16 14:45:38 +0300

    + Added: adding new project from Projects view

--- ProcessManager.UI/ViewModels/MainWindowViewModel.cs
+++ ProcessManager.UI/ViewModels/MainWindowViewModel.cs
@@ -1,39 +1,43 @@
 namespace ProcessManager.UI
 {
+    using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Diagnostics;
     using System.IO;
     using System.Linq;
+    using System.Text;
+    using System.Text.Json;
     using System.Windows.Controls;
+    using System.Windows.Input;
 
     /// <summary>
     /// The ViewModel for MainWindow
     /// </summary>
     public class MainWindowViewModel : BaseViewModel
     {
-        public static MainWindowViewModel DesignInstance => new MainWindowViewModel(
-            new Project[] 
+        public static MainWindowViewModel MainViewDesignInstance => new MainWindowViewModel()
+        {
+            Projects = new ObservableCollection<ProjectItemViewModel>()
             {
-                new Project()
+                new ProjectItemViewModel(new Project()
                 {
                     ProjectPath = $@"C:\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}",
-                },
-
-                new Project()
-               {
-                   ProjectPath = $@"D:\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}",
-               },
-
-                new Project()
-               {
-                   ProjectPath = $@"A:\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}",
-               },
-            })
-        {
-
-            Projects = new ProjectItemViewModel[] { },
-            CurrentView = new MainView(new MainWindowViewModel(new Project[] { })),
+                    ProcessList = new List<ProcessModel>(),
+                }),
+                new ProjectItemViewModel(new Project()
+                {
+                    ProjectPath = $@"D:\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}",
+                    ProcessList = new List<ProcessModel>(),
+                }),
+                new ProjectItemViewModel(new Project()
+                {
+                    ProjectPath = $@"A:\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}\{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}",
+                    ProcessList = new List<ProcessModel>(),
+                }),
+            },
         };
+
 
 
         #region Private fields
@@ -49,7 +53,7 @@ namespace ProcessManager.UI
         /// <summary>
         /// A list of projects
         /// </summary>
-        public IEnumerable<ProjectItemViewModel> Projects { get; set; }
+        public ObservableCollection<ProjectItemViewModel> Projects { get; set; }
 
         /// <summary>
         /// The page currently being displayed
@@ -67,12 +71,68 @@ namespace ProcessManager.UI
         #endregion
 
 
+        #region Public commands
+
+        public ICommand AddNewProjectCommnad { get; }
+
+        #endregion
+
+
+        /// <summary>
+        /// The only reason for this constructor to exist is so the DesignTime singelton would actually work in the designer
+        /// </summary>
+        private MainWindowViewModel() { }
+
         public MainWindowViewModel(IEnumerable<Project> projects)
         {
-            Projects = projects.Select(project =>
+            Projects = new ObservableCollection<ProjectItemViewModel>(projects.Select(project =>
             {
                 return new ProjectItemViewModel(project);
+            }));
+
+            AddNewProjectCommnad = new RelayCommand(ExecuteAddNewProjectCommnad);
+        }
+
+
+        private void ExecuteAddNewProjectCommnad()
+        {
+            string selectedFolder = DI.FolderDialog.ShowDialog();
+
+            if (string.IsNullOrWhiteSpace(selectedFolder))
+                return;
+
+            string configPath = Path.Combine(selectedFolder, Localization.PROJECT_CONFIG_FILE_NAME);
+
+            // If folder already contains Project.Config file
+            if (File.Exists(configPath))
+                return;
+
+            // Create Project.Config 
+            File.Create(configPath).Dispose();
+
+            // Write empty json brackets to file
+            File.WriteAllText(configPath, "[\n\n]");
+
+            var newProject = new Project()
+            {
+                ProjectPath = selectedFolder,
+                ProcessList = new List<ProcessModel>(),
+            };
+
+            DI.Projects.Add(newProject);
+
+            var jsonString = JsonSerializer.Serialize(DI.Projects.Select(project => new
+            {
+                ProjectPath = project.ProjectPath,
+            }), new JsonSerializerOptions()
+            {
+                WriteIndented = true,
             });
+
+
+            File.WriteAllText(Localization.PROJECTS_FILE_NAME, jsonString);
+
+            Projects.Add(new ProjectItemViewModel(newProject));
         }
 
     };
