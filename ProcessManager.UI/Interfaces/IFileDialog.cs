eb99ad6258000da39c1168f26237391549406853
Josef Lintz <yosi1.lints@gmail.com>
2020-04-19 12:49:24 +0300

    + Added IFileDialog and windows implementation

---
+++ ProcessManager.UI/Interfaces/IFileDialog.cs
@@ -0,0 +1,17 @@
+﻿namespace ProcessManager.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    /// <summary>
+    ///
+    /// </summary>
+    public interface IFileDialog
+    {
+        public string GetOpenFileDialogRresult();
+        public bool ShowOpenFileDialog();
+
+    };
+};
---
+++ ProcessManager.UI/Models/WindowsFileDialog.cs
@@ -0,0 +1,24 @@
+﻿namespace ProcessManager.UI
+{
+    using Microsoft.Win32;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WindowsFileDialog : IFileDialog
+    {
+        OpenFileDialog _openFileDialog = new OpenFileDialog();
+
+        public string GetOpenFileDialogRresult()
+        {
+            return _openFileDialog.FileName;
+        }
+
+        public bool ShowOpenFileDialog()
+        {
+            return _openFileDialog.ShowDialog().Value;
+        }
+
+    };
+};
