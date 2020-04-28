076af31b9cb0497a100e18bad4bb61863c70ac03
Josef Lintz <yosi1.lints@gmail.com>
2020-04-28 18:18:41 +0300

    + Added better handling when user select project (directory) that contains a project config file

--- ProcessManager.UI/ViewModels/Project Viewmodels/ProjectsListViewModel.cs
+++ ProcessManager.UI/ViewModels/Project Viewmodels/ProjectsListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace ProcessManager.UI
 {
+    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Diagnostics;
@@ -92,35 +93,63 @@
         private void ExecuteAddNewProjectCommnad()
         {
             var folderDialog = DI.FolderDialog;
+
+            // Show select folder dialog
             var selectedFolderResult = folderDialog.ShowDialog();
 
+            // If user canceled selection
             if (selectedFolderResult == false)
                 return;
 
+            // Combine the selected path and the config file name to get the path to the new config file
             string configPath = Path.Combine(folderDialog.SelectedPath, Localization.PROJECT_CONFIG_FILE_NAME);
 
             // If folder already contains Project.Config file
             if (File.Exists(configPath))
             {
-                DI.UserDialog.ShowDialog($"Selected folder already contains a \'{Localization.PROJECT_CONFIG_FILE_NAME}\' file");
-                return;
+                // Ask user if he'd like to add the project to the app
+                var result = DI.UserDialog.ShowChoiceDialog($"Selected folder already contains a \'{Localization.PROJECT_CONFIG_FILE_NAME}\' file.\n" +
+                                                            $"Would you like to add it ?");
+            
+                // If user clicked yes
+                if (result == UserDialogResult.Yes)
+                {
+                    // Add the project to app
+                    AddProjectToApp(folderDialog.SelectedPath, configPath);
+                };
+            }
+            else
+            {
+                // Create empty config file
+                CreateEmptyConfigFile(configPath);
+
+                // Add project to app
+                AddProjectToApp(folderDialog.SelectedPath, configPath);
             };
 
-            // Create Project.Config 
-            File.Create(configPath).Dispose();
+        }
 
-            // Write empty json brackets to file
-            File.WriteAllText(configPath, "[\n\n]");
+
+        /// <summary>
+        /// Adds a Project's config file to the app
+        /// </summary>
+        /// <param name="selectedPath"> A path to a project directory that the user has selected </param>
+        /// <param name="configPath"> A path that combines the selected path and the filename of the config file </param>
+        private void AddProjectToApp(string selectedPath, string configPath)
+        {
+            // Read process info from config file
+            var processList = DI.ProcessLoader.GetProcessListFromFile(configPath);
 
             // Create the new project
             var newProject = new Project()
             {
-                ProjectPath = folderDialog.SelectedPath,
-                ProcessList = new List<IProcessModel>(),
+                ProjectPath = selectedPath,
+                ProcessList = new List<IProcessModel>(processList),
             };
 
             // Add the new project to the projects list
             DI.Projects.Add(newProject);
+
 
             // Convert the new projects list to a json string
             var jsonString = JsonSerializer.Serialize(DI.Projects.Select(project => new
@@ -135,13 +164,27 @@
             // Write the json to file
             File.WriteAllText(Localization.PROJECTS_FILE_NAME, jsonString);
 
-
-            // Add the project to this ViewModel's projects list
-            Projects.Add(new ProjectItemViewModel(newProject));
-
             // Update tray icon
             DI.SystemTrayIcon.RemoveIcon();
             DI.SetupTrayIcon(new WindowInteropHelper(App.Current.MainWindow).Handle);
+
+
+            // Add the project to this ViewModel's projects list
+            Projects.Add(new ProjectItemViewModel(newProject));
+        }
+
+
+        /// <summary>
+        /// Create an empty <see cref="Localization.PROJECT_CONFIG_FILE_NAME"/>
+        /// </summary>
+        /// <param name="configPath"></param>
+        private void CreateEmptyConfigFile(string configPath)
+        {
+            // Create Project.Config 
+            File.Create(configPath).Dispose();
+
+            // Write empty json brackets to file
+            File.WriteAllText(configPath, "[\n\n]");
         }
 
     };
