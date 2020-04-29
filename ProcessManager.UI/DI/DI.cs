b9a0f323165b1d3db6249cf35c8fe294513a9bdb
Josef Lintz <yosi1.lints@gmail.com>
2020-04-29 14:29:01 +0300

    - Removed TrayIcon setup function from DI

--- ProcessManager.UI/DI/DI.cs
+++ ProcessManager.UI/DI/DI.cs
@@ -2,93 +2,33 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Linq;
-    using System.Runtime.InteropServices;
 
     /// <summary>
     /// Simple DI container
     /// </summary>
     public static class DI
     {
-        /// <summary>
-        /// The process list loaded from file
-        /// </summary>
-        public static List<IProcessModel> ProcessList { get; set; }
+        public static IServiceProvider Provider { get; set; }
+
 
         /// <summary>
         /// The process list file loader/reader
         /// </summary>
-        public static IProcessLoader ProcessLoader { get; set; }
+        public static IProcessLoader ProcessLoader => GetService<IProcessLoader>();
 
-        public static IList<Project> Projects { get; set; }
+        public static IList<Project> Projects => GetService<IList<Project>>();
 
-        public static IProjectLoader ProjectLoader { get; set; }
+        public static IProjectLoader ProjectLoader => GetService<IProjectLoader>();
 
-        public static MainWindowViewModel MainWindowViewModel { get; set; }
+        public static MainWindowViewModel MainWindowViewModel => GetService<MainWindowViewModel>();
 
-        public static SystemTrayIcon SystemTrayIcon { get; set; }
-
-
-        public static IFolderDialog FolderDialog => new WindowsFolderDialog();
-        public static IFileDialog FileDialog => new WindowsFileDialog();
-
-        public static IUserDialog UserDialog => new WindowsUserDialog();
+        public static SystemTrayIcon SystemTrayIcon => GetService<SystemTrayIcon>();
 
 
-        /// <summary>
-        /// Creates a TrayIcon, and send it some data
-        /// </summary>
-        /// <param name="hwnd"></param>
-        public static void SetupTrayIcon(IntPtr hwnd)
-        {
-            // Create the SystemTrayIcon class
-            SystemTrayIcon = new SystemTrayIcon(Localization.APP_ICON_LOCATION, hwnd);
+        public static IFolderDialog FolderDialog => GetService<IFolderDialog>(); 
+        public static IFileDialog FileDialog => GetService<IFileDialog>(); 
 
-            // Initialize the tray icon and give it the neccessary project data
-            SystemTrayIcon.CreateIcon(Projects
-            .Select(project =>
-            {
-                // The data which will be passed to the tray icon
-                var trayIconData = new SystemTrayIconData(project);
-
-                // A local function that will take a handle and "convert" it to a usable object
-                static T HandleToObj<T>(IntPtr handle)
-                {
-                    // Take the handle and convert it to a *safe handle
-                    if (GCHandle.FromIntPtr(handle).Target is T obj)
-                        return obj;
-                    else
-                    {
-                        Debugger.Break();
-                        throw new Exception("Critical error occured. Unable to read returned TrayIcon data");
-                    };
-                };
-
-                // A callback that will be called when the user decided to close the project
-                trayIconData.CloseProjectCallBack += (data) =>
-                {
-                    Project project = HandleToObj<Project>(data);
-                    project.CloseProjectTree();
-                };
-
-                // A callback that will be called when the user decided to run the project
-                trayIconData.RunProjectCallBack += (data) =>
-                {
-                    Project project = HandleToObj<Project>(data);
-                    project.RunProject();
-                };
-
-                return trayIconData;
-
-            })
-            // Convert to an array Because
-            .ToArray());
-
-
-            // Actually show the icon
-            SystemTrayIcon.ShowIcon();
-        }
-
+        public static IUserDialog UserDialog => GetService<IUserDialog>();
+        
     };
 };
