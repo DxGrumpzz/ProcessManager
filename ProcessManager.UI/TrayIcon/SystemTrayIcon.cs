eccf9be566f44c557e91797fe24d2df9cf5e8727
Josef Lintz <yosi1.lints@gmail.com>
2020-04-29 14:21:30 +0300

    * Changed SystemTrayIcon object initialization

--- ProcessManager.UI/TrayIcon/SystemTrayIcon.cs
+++ ProcessManager.UI/TrayIcon/SystemTrayIcon.cs
@@ -7,37 +7,51 @@
     /// </summary>
     public class SystemTrayIcon
     {
-
-        /// <summary>
-        /// Path to the icon's bitmap
-        /// </summary>
-        private readonly string _iconPath;
-
-        /// <summary>
-        /// A handle to the main window
-        /// </summary>
-        private readonly IntPtr _mainWindowHandle;
-
         /// <summary>
         /// The Icon's pointer/handle
         /// </summary>
         private IntPtr _iconPointer;
 
+        private IntPtr _mainWindowHandle;
 
-        public SystemTrayIcon(string iconPath, IntPtr mainWindowHandle)
+        private Func<SystemTrayIconData[]> _iconDataFactory;
+
+        private string _iconPath;
+
+
+        public SystemTrayIcon(Func<SystemTrayIconData[]> iconDataFactory)
         {
-            _iconPath = iconPath;
-            _mainWindowHandle = mainWindowHandle;
+            if (iconDataFactory is null)
+                throw new ArgumentNullException(nameof(iconDataFactory));
+
+            _iconDataFactory = iconDataFactory;
         }
+
 
         /// <summary>
         /// *Creates* the icon and gives it some data
         /// </summary>
         /// <param name="systemTrayIconData"></param>
-        public void CreateIcon(SystemTrayIconData[] systemTrayIconData)
+        public void BuildIcon(string iconPath, IntPtr mainWindowHandle)
         {
+            _iconPath = iconPath;
+            _mainWindowHandle = mainWindowHandle;
+
+            var trayIconData = _iconDataFactory();
+
             // Call C++ dll and create the icon. 
-            _iconPointer = CoreDLL.CreateSystemTrayIcon(_mainWindowHandle, _iconPath, systemTrayIconData, systemTrayIconData.Length);
+            _iconPointer = CoreDLL.CreateSystemTrayIcon(mainWindowHandle, iconPath, trayIconData, trayIconData.Length);
+        }
+
+
+        public void RebuildIcon(bool showAfterBuild = false)
+        {
+            RemoveIcon();
+            BuildIcon(_iconPath, _mainWindowHandle);
+
+
+            if (showAfterBuild == true)
+                ShowIcon();
         }
 
         /// <summary>
