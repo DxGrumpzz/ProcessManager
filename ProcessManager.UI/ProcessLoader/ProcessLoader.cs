d4ec5d4df598ca52778a0dabf53e36df7453bf6f
Josef Lintz <yosi1.lints@gmail.com>
2020-04-30 14:47:32 +0300

    * Renamed JsonProcessLoader to ProcessLoader

--- ProcessManager.UI/ProcessLoader/ProcessLoader.cs
+++ ProcessManager.UI/ProcessLoader/ProcessLoader.cs
@@ -1,116 +1,54 @@
 ﻿namespace ProcessManager.UI
 {
+    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Text.Json;
+    using System.Windows;
 
     /// <summary>
-    /// The default implementation of <see cref="IProcessLoader"/>
+    /// A json process file loader
     /// </summary>
     public class ProcessLoader : IProcessLoader
     {
-        private string[] _fileLines;
+        private readonly ISerializer _serializer;
 
-        public ProcessLoader(string filePath)
+        public ProcessLoader(ISerializer serializer)
         {
-            // Read the file line-by-line
-            _fileLines = File.ReadAllLines(filePath)
-                // Remove empty lines
-                .Where(line => string.IsNullOrWhiteSpace(line) == false)
-                .ToArray();
+            _serializer = serializer;
         }
-
 
         /// <summary>
         /// Retrieves an <see cref="IEnumerable{T}"/> of ProcessModel from a file
         /// </summary>
-        /// <param name="filePath"> The path to the file </param>
         /// <returns></returns>
-        public IEnumerable<ProcessModel> GetProcessListFromFile()
+        public IEnumerable<IProcessModel> GetProcessListFromFile(string processListFile)
         {
-            // Stores the processes
-            List<ProcessModel> processList = new List<ProcessModel>();
+            // Read json data from file 
+            var json = File.ReadAllText(processListFile);
 
+            // Parse json data
+            var jsonData = _serializer.DeserializeFromString<IEnumerable<JsonProcessModel>>(json);
 
-            for (int a = 0; a < _fileLines.Length; a++)
+            return jsonData.Select<JsonProcessModel, IProcessModel>(jsonProcess =>
             {
-                // Hold the current line 
-                string currentLine = _fileLines[a].ToLower();
-
-                // If the current line specifies a process path
-                if (currentLine == "[process]")
+                if (jsonProcess.RunAsConsole == true)
                 {
-                    // Add a new ProcessModel to the list with the name of the process
-                    processList.Add(new ProcessModel()
+                    return new ConsoleProcess(jsonProcess.ConsoleScript, jsonProcess.StartInDirectory, jsonProcess.VisibleOnStartup)
                     {
-                        ProcessPath = _fileLines[++a],
-                    });
+                        ProcessLabel = jsonProcess.ProcessLabel,
+                    };
                 }
-                // If the current line specifies a process' arguments
-                else if (currentLine == "[args]")
+                else
                 {
-                    // Get the last process added and set it's arguments to the next line
-                    processList.Last().ProcessArgs = _fileLines[++a];
+                    return new GUIProcess(jsonProcess.ProcessPath, jsonProcess.ProcessArgs, jsonProcess.VisibleOnStartup)
+                    {
+                        ProcessLabel = jsonProcess.ProcessLabel,
+                    };
                 };
-            };
 
-            return processList;
+            });
         }
-
-
-        /// <summary>
-        /// Check if Processes.txt is valid
-        /// </summary>
-        /// <param name="fileData"></param>
-        /// <returns></returns>
-        public bool IsProcessesFileFileValid()
-        {
-            // The process file must contain at least 2 lines for it to be valid
-            if (_fileLines.Length < 2)
-                return false;
-
-            // The [processes] keyword must show up at least once
-            if (_fileLines.Count(line => line.ToLower() == "[process]") < 1)
-                return false;
-
-            // Get processes 
-            var processPaths = GetProcessPaths(_fileLines);
-
-            // Check if the process paths are valid
-            foreach (string processPath in processPaths)
-            {
-                if (File.Exists(processPath) == false)
-                {
-                    return false;
-                };
-            };
-
-            // We reached here, Processes file is valid
-            return true;
-        }
-
-
-        /// <summary>
-        /// Reads the processes file and retrieves the process paths
-        /// </summary>
-        /// <param name="fileData"> The porcess file inner text </param>
-        /// <returns></returns>
-        private IEnumerable<string> GetProcessPaths(string[] fileData)
-        {
-            // Go through every line
-            for (int a = 0; a < fileData.Length; a++)
-            {
-                // Get the current line being read
-                string currentLine = fileData[a].ToLower();
-
-                // If the current line specifies a process
-                if(currentLine == "[process]")
-                {
-                    // Read the next line and "return" the process path
-                    yield return fileData[++a]; 
-                };
-            };
-        }
-
     };
 };
