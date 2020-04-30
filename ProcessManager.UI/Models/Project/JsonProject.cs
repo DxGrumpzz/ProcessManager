ab71a0cbb32074f0447079c1e855720f6c0d955e
Josef Lintz <yosi1.lints@gmail.com>
2020-04-30 14:43:31 +0300

    + Added JsonProject struct

---
+++ ProcessManager.UI/Models/Project/JsonProject.cs
@@ -0,0 +1,12 @@
+﻿namespace ProcessManager.UI
+{
+    using System;
+
+    /// <summary>
+    /// A simple struct containing the structure of a <see cref="Project"/> as it would appear as json
+    /// </summary>
+    public struct JsonProject
+    {
+        public string ProjectPath { get; set; }
+    };
+};
