b81c5695d1c8f155504c3057ec4c004193be92cf
Josef Lintz <yosi1.lints@gmail.com>
2020-05-03 15:56:06 +0300

    + Added SerializeProcessList function to ISerializerExtensions

--- ProcessManager.UI/Extension methods/ISerializerExtensions.cs
+++ ProcessManager.UI/Extension methods/ISerializerExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace ProcessManager.UI
 {
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Linq;
 
 
@@ -39,6 +40,83 @@
             var projects = serializer.Deserialize<IEnumerable<Project>>(jsonProjectData);
 
             return projects;
+        }
+
+
+        public static byte[] SerializeProcessList(this ISerializer serializer, IList<IProcessModel> ProcessList)
+        {
+            byte[] projectBytes = default;
+
+            if (serializer is JsonSerializer jsonSerializer)
+            {
+                projectBytes = serializer.Serialize(
+                    ProcessList
+                    .Select(process =>
+                    {
+                        switch (process)
+                        {
+                            case ConsoleProcess consoleProcess:
+                                return ConsoleProcessAsJsonProcess(consoleProcess);
+
+
+                            case GUIProcess guiProcess:
+                                return GUIProcessAsJsonProcess(guiProcess);
+
+                            default:
+                            {
+                                Debugger.Break();
+                                return default;
+                            };
+                        };
+                    }));
+            };
+
+
+            return projectBytes;
+        }
+
+
+
+
+
+        /// <summary>
+        /// Converts a <see cref="GUIProcess"/> to a <see cref="ConsoleProcess"/> 
+        /// </summary>
+        /// <param name="consoleProcess"></param>
+        /// <returns></returns>
+        private static JsonProcessModel ConsoleProcessAsJsonProcess(ConsoleProcess consoleProcess)
+        {
+            return new JsonProcessModel
+            {
+                RunAsConsole = true,
+
+                VisibleOnStartup = consoleProcess.VisibleOnStartup,
+
+                StartInDirectory = consoleProcess.StartupDirectory,
+                ConsoleScript = consoleProcess.ConsoleScript,
+
+                ProcessLabel = consoleProcess.ProcessLabel,
+            };
+        }
+
+        /// <summary>
+        /// Converts a <see cref="GUIProcess"/> to a <see cref="JsonProcessModel"/> 
+        /// </summary>
+        /// <param name="consoleProcess"></param>
+        /// <returns></returns>
+        private static JsonProcessModel GUIProcessAsJsonProcess(GUIProcess guiProcess)
+        {
+            return new JsonProcessModel
+            {
+                RunAsConsole = false,
+
+                VisibleOnStartup = guiProcess.VisibleOnStartup,
+
+                ProcessPath = guiProcess.ProcessPath,
+                ProcessArgs = guiProcess.ProcessArgs,
+
+                ProcessLabel = guiProcess.ProcessLabel,
+            };
         }
 
     };
