13dc4ffac6889b5adc0b4c8f3fcec141cec14570
Josef Lintz <yosi1.lints@gmail.com>
2020-05-04 15:42:08 +0300

    + Added ProcessTypeToEditTextConverter

---
+++ ProcessManager.UI/ValueConverters/ProcessTypeToEditTextConverter.cs
@@ -0,0 +1,36 @@
+﻿namespace ProcessManager.UI
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+
+
+    /// <summary>
+    /// A value converter that takes a <see cref="ProcessType"/> and returns a string based on it's value
+    /// </summary>
+    public class ProcessTypeToEditTextConverter : BaseValueConverter<ProcessTypeToEditTextConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((ProcessType)value)
+            {
+                case ProcessType.Console:
+                    return "Edit as console";
+
+                case ProcessType.GUI:
+                    return "Edit as GUI";
+
+                default:
+                {
+                    Debugger.Break();
+                    return null;
+                };
+            };
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    };
+};
