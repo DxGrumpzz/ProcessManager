102044d924c438bb84670b3606a62110b7eb7f23
Josef Lintz <yosi1.lints@gmail.com>
2020-05-13 20:02:31 +0300

    + Added ProcessTypeToImageValueConverter value covnerter

---
+++ ProcessManager.UI/ValueConverters/ProcessType converters/ProcessTypeToImageValueConverter.cs
@@ -0,0 +1,37 @@
+﻿namespace ProcessManager.UI
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProcessTypeToImageValueConverter : BaseValueConverter<ProcessTypeToImageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((ProcessType)value)
+            {
+                case ProcessType.Console:
+                {
+                    return App.Current.Resources["GUIProcessImage"];
+                };
+
+                case ProcessType.GUI:
+                {
+                    return App.Current.Resources["ConsoleProcessImage"];
+                };
+
+                default:
+                    Debugger.Break();
+                    return null;
+            };
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    };
+};
