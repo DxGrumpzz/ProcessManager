11c5106c82f969b932d33c3439db5c3e37938629
Josef Lintz <yosi1.lints@gmail.com>
2020-05-14 14:57:50 +0300

    + Added BuildResourceExtentions attached property

---
+++ ProcessManager.UI/AttachedProperties/BuildResourceExtentions.cs
@@ -0,0 +1,25 @@
+﻿namespace ProcessManager.UI
+{
+    using System.Windows;
+
+    /// <summary>
+    /// An attached property that is used in BuildResourceExtentions project 
+    /// </summary>
+    public class BuildResourceExtentions
+    {
+
+        public static bool GetLoadIntoFile(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(LoadIntoFileAttachedProperty);
+        }
+
+        public static void SetLoadIntoFile(DependencyObject obj, bool value)
+        {
+            obj.SetValue(LoadIntoFileAttachedProperty, value);
+        }
+
+        public static readonly DependencyProperty LoadIntoFileAttachedProperty =
+            DependencyProperty.RegisterAttached("LoadIntoFile", typeof(bool), typeof(BuildResourceExtentions), new PropertyMetadata(false));
+
+    };
+};
