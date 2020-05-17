5f69e3815db0d1297993c56fe2443ccfe268b7ad
Josef Lintz <yosi1.lints@gmail.com>
2020-05-17 16:59:33 +0300

    + Added comments to IUIManager and view

--- ProcessManager.UI/Interfaces/IUIManager.cs
+++ ProcessManager.UI/Interfaces/IUIManager.cs
@@ -4,29 +4,20 @@
     using System.Collections.Generic;
     using System.Text;
 
-    public enum View
-    {
-        ProjectsListView = 0,
-        ProjectItemView = 1,
-
-        EditGUIProcessView = 2,
-        EditConsoleProcessView = 3,
-
-        AddGUIProcessView = 4,
-        AddConsoleProcessView = 5,
-
-        AddProcessView = 6,
-    };
-
 
     /// <summary>
-    /// 
+    /// An interface that describes an implementation of a WPF ui manager
     /// </summary>
     public interface IUIManager
     {
-
-        public void ChangeView<T>(View view, T viewmodel) 
-            where T : BaseViewModel;
+        /// <summary>
+        /// Changes the app's current view 
+        /// </summary>
+        /// <typeparam name="T"> The type of viewmodel </typeparam>
+        /// <param name="view"> The view to change to </param>
+        /// <param name="viewmodel"> The view's associated viewmodel </param>
+        public void ChangeView<TViewModel>(View view, TViewModel viewmodel) 
+            where TViewModel : BaseViewModel;
 
     };
 };
---
+++ ProcessManager.UI/Models/Enums/View.cs
@@ -0,0 +1,44 @@
+﻿namespace ProcessManager.UI
+{
+    /// <summary>
+    /// An enum for every view available to switch to 
+    /// </summary>
+    public enum View
+    {
+        /// <summary>
+        /// The apps main page
+        /// </summary>
+        ProjectsListView = 0,
+
+        /// <summary>
+        /// A view for a project, contains processes, and project settings
+        /// </summary>
+        ProjectItemView = 1,
+
+        /// <summary>
+        /// A view that allows editing a GUI process
+        /// </summary>
+        EditGUIProcessView = 2,
+
+        /// <summary>
+        /// A view that allows editing a Console process
+        /// </summary>
+        EditConsoleProcessView = 3,
+
+        /// <summary>
+        /// A view for adding a GUI process to a project
+        /// </summary>
+        AddGUIProcessView = 4,
+
+        /// <summary>
+        /// A view for adding a Console process to a project
+        /// </summary>
+        AddConsoleProcessView = 5,
+
+        /// <summary>
+        /// A view that allows the user to select a process type to add 
+        /// </summary>
+        AddProcessView = 6,
+
+    };
+};
