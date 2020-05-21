96038472ce68cc93f94db22120c7d38797a64071
Josef Lintz <yosi1.lints@gmail.com>
2020-05-21 16:39:15 +0300

    * Moved drag and drop stuff from ProjectListView Code-Behind

--- ProcessManager.UI/ViewModels/Project Viewmodels/ProjectsListItemViewModel.cs
+++ ProcessManager.UI/ViewModels/Project Viewmodels/ProjectsListItemViewModel.cs
@@ -1,6 +1,9 @@
 ﻿namespace ProcessManager.UI
 {
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Data;
+    using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
@@ -11,6 +14,7 @@
     /// </summary>
     public class ProjectListItemViewModel : BaseViewModel
     {
+
         public static ProjectListItemViewModel DesignInstance => new ProjectListItemViewModel(
             new Project()
             {
@@ -20,11 +24,21 @@
             SettingsButtonVisible = true,
         };
 
+        private const string DRAG_DROP_DATA_NAME = "ViewModelData";
 
-        public Project Project { get; }
+
+
+        #region Private fields
+
+        private bool _isSelected;
 
         private bool _settingsButtonVisible;
 
+        #endregion
+
+
+
+        #region Public properties
 
         public bool SettingsButtonVisible
         {
@@ -36,10 +50,41 @@
             }
         }
 
+        /// <summary>
+        /// A boolean flag that indicates that the "something" is over the control, the user's mouse, a draggable element, and such
+        /// </summary>
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Project Project { get; }
+
+        public Dictionary<string, object> DragDropData { get; }
+
+        private bool _dragAndDropEnabled;
+
+        public bool DragAndDropEnabled
+        {
+            get => _dragAndDropEnabled;
+            set
+            {
+                _dragAndDropEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #endregion
+
 
 
         #region Public commands
-
 
         public ICommand RunProjectCommand { get; }
         public ICommand CloseProjectCommand { get; }
@@ -49,8 +94,16 @@
 
         public ICommand SwitchToProjectViewCommand { get; }
 
-        #endregion
 
+        public ICommand DragEnterCommand { get; }
+        public ICommand DragLeaveCommand { get; }
+        public ICommand DropCommand { get; }
+
+        public ICommand DragCommand  { get; }
+        
+        public ICommand MouseMovedCommand { get; }
+
+        #endregion
 
 
         private ProjectListItemViewModel() { }
@@ -58,19 +111,84 @@
         {
             Project = project;
 
+            DragDropData = new Dictionary<string, object>()
+            {
+                { DRAG_DROP_DATA_NAME, this },
+            };
+
+
             RunProjectCommand = new RelayCommand(ExecuteRunProjectCommand, singleFire: true);
             CloseProjectCommand = new RelayCommand(ExecuteCloseProjectCommand, singleFire: true);
 
             SwitchToProjectViewCommand = new RelayCommand(ExecuteSwitchToProjectViewCommand);
 
+
+            // Bind drag and drop events
+            DragEnterCommand = new RelayCommand<ProjectListItemViewModel>((project) =>
+            {
+                IsSelected = true;
+            });
+
+            DragLeaveCommand = new RelayCommand<ProjectListItemViewModel>((project) =>
+            {
+                IsSelected = false;
+            });
+
+            DropCommand = new RelayCommand<Dictionary<string, object>>((dragDropData) =>
+            {
+                if (dragDropData is null)
+                {
+                    DI.Logger.Log("Drag and drop error. Dropped data is null", LogLevel.Warning);
+                    Debugger.Break();
+                    return;
+                };
+
+                if (dragDropData.TryGetValue(DRAG_DROP_DATA_NAME, out object dropData) == false)
+                {
+                    DI.Logger.Log("Drag and drop error. Unable to find valid data", LogLevel.Error);
+                    Debugger.Break();
+                    return;
+                };
+
+                if (!(dropData is ProjectListItemViewModel dropDataAsVM))
+                {
+                    DI.Logger.Log("Drag and drop error. Dropped data contains invald value(s)", LogLevel.Warning);
+                    Debugger.Break();
+                    return;
+                };
+
+                Drop(dropDataAsVM);
+            });
+
+
+
+            // Bind mouse commands
+
+            // When the mouse moves over the drag drop button
+            MouseMovedCommand = new RelayCommand<MouseMovedInfo>((mouseInfo) =>
+            {
+                // Check if user held his left mouse button
+                if (mouseInfo.LeftButtonPressed == true)
+                {
+                    // Enable drag drop
+                    DragAndDropEnabled = true;
+                }
+                else
+                {
+                    DragAndDropEnabled = false;
+                };
+            });
+
             MouseEnterCommand = new RelayCommand(() =>
             {
                 SettingsButtonVisible = true;
+                IsSelected = true;
             });
 
             MouseLeaveCommand = new RelayCommand(() =>
             {
                 SettingsButtonVisible = false;
+                IsSelected = false;
             });
         }
 
--- ProcessManager.UI/Views/Project views/ProjectListView.xaml.cs
+++ ProcessManager.UI/Views/Project views/ProjectListView.xaml.cs
@@ -1,22 +1,16 @@
 ﻿namespace ProcessManager.UI
 {
-    using System.Collections.ObjectModel;
+    using System.Collections.Generic;
     using System.Diagnostics;
-    using System.IO;
-    using System.Linq;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
-    using System.Windows.Media;
 
     /// <summary>
     /// Interaction logic for MainView.xaml
     /// </summary>
     public partial class ProjectListView : UserControl
     {
-        private ProjectsListViewModel _viewModel;
-
-        private const string DRAG_DROP_DATA_NAME = "ViewModelData";
 
         public ProjectListView()
         {
@@ -25,59 +19,8 @@
 
         public ProjectListView(ProjectsListViewModel viewModel)
         {
-            InitializeComponent();
             DataContext = viewModel;
-            _viewModel = viewModel;
-        }
-
-
-        private void MainBorder_Drop(object sender, DragEventArgs e)
-        {
-            Border border = (Border)sender;
-            var currentData = (ProjectListItemViewModel)border.DataContext;
-
-            var data = e.Data.GetData(DRAG_DROP_DATA_NAME);
-
-            if (data is null)
-                return;
-
-            if (!(data is ProjectListItemViewModel droppedData))
-            {
-                Debugger.Break();
-                return;
-            };
-
-            if (currentData == data)
-                return;
-
-            currentData.Drop(droppedData);
-        }
-
-
-        private void Button_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                Button border = (Button)sender;
-
-                DataObject dragDropData = new DataObject();
-
-                dragDropData.SetData(DRAG_DROP_DATA_NAME, border.DataContext);
-
-                DragDrop.DoDragDrop(border, dragDropData, DragDropEffects.Move);
-            };
-        }
-
-        private void MainBorder_DragEnter(object sender, DragEventArgs e)
-        {
-            Border border = (Border)sender;
-            border.Background = (SolidColorBrush)App.Current.Resources["LightGreyBrush"];
-        }
-
-        private void MainBorder_DragLeave(object sender, DragEventArgs e)
-        {
-            Border border = (Border)sender;
-            border.Background = Brushes.Transparent;
+            InitializeComponent();
         }
     }
 }
